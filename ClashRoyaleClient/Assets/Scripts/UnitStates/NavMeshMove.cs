1d4b3ee271d94ce71bea9c37ed186dbf2b4c7340
AlexanderFredy <117285141+AlexanderFredy@users.noreply.github.com>
2024-04-12 13:55:30 +0300

    Base scene, navmesh surface, warior move behaviour

---
+++ ClashRoyaleClient/Assets/Scripts/UnitStates/NavMeshMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "NavMeshMove", menuName = "UnitState/NavMeshMove")]
+public class NavMeshMove : UnitState
+{
+    [SerializeField] private float _moveOffset = 0.5f;
+    private NavMeshAgent _agent;
+    private Vector3 _targetPosition;
+
+    public override void Init()
+    {
+        _agent = _unit.GetComponent<NavMeshAgent>();
+        _targetPosition = Vector3.forward;
+        _agent.SetDestination(_targetPosition);
+    }
+    public override void Run()
+    {
+        float distanceToTarget = Vector3.Distance(_unit.transform.position, _targetPosition);
+        if (distanceToTarget <= _moveOffset) { Debug.Log("Done"); }
+        
+    }
+
+    public override void Finish()
+    {
+        _agent.isStopped = true;
+    }
+
+}
