bfde89148cee7a1e0fa2421b56e61832699237da
AlexanderFredy <117285141+AlexanderFredy@users.noreply.github.com>
2024-04-25 20:41:02 +0300

    server and registaration account from unity

---
+++ ClashRoyaleClient/Assets/Scripts/Authorization.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Authorization : MonoBehaviour
+{
+    private const string LOGIN = "login";
+    private const string PASSWORD = "password";
+
+    private string _login;
+    private string _password;
+
+    public event Action Error;
+
+    public void SetLogin(string login)
+    {
+        _login = login;
+    }
+
+    public void SetPassword(string password)
+    {
+        _password = password;
+    }
+
+    public void SignIn()
+    {
+        if (string.IsNullOrEmpty(_login) || string.IsNullOrEmpty(_password))
+        {
+            ErrorMessage("Login and/or password is empty");
+            return;
+        }
+
+        string uri = URILibrary.MAIN + URILibrary.AUTHORIZATION;
+        Dictionary<string, string> data = new Dictionary<string, string>()
+        {
+            {LOGIN, _login},
+            {PASSWORD, _password},
+        };
+        WebRequestToMySQL.Instance.StartPost(uri,data,Success, ErrorMessage);
+    }
+
+    private void Success(string data)
+    {
+        string[] result = data.Split('|');
+        if (result.Length < 2 || result[0] != "ok")
+        {
+            ErrorMessage("Server answer: " + data);
+            return;
+        }
+
+        if (int.TryParse(result[1], out int id))
+        {
+            UserInfo.Instance.SetID(id);
+            Debug.Log("success for id:" + id);
+        }
+        else { 
+            ErrorMessage($"Can't try parse \"{result[1]}\" in integer. Full answer is: {data}");
+        }
+    }
+
+    private void ErrorMessage(string errror)
+    {
+        Debug.LogError(errror);
+        Error?.Invoke();
+    }
+}
---
+++ ClashRoyaleClient/Assets/Scripts/URILibrary.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class URILibrary : MonoBehaviour
+{
+    public const string MAIN = "http://localhost/Projects/LessonDataBase/";
+    public const string AUTHORIZATION = "authorization.php";
+    public const string REGISTRATION = "registration.php";
+}
